012fd0af14ced071d9a135b99973d5bb54516951
Ludovic Barre <ludovic.barre@foss.st.com>
2025-09-04 15:53:02 +0200

    app: copro: add thread flags support to start and stop command
    Add thread flags support to start and stop command.
    
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Change-Id: Iceea77627ee0a339fa8682203ef6d64d4b855805
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/TF-M/tf-m-tests/+/487195
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>

--- app_broker/copro_task.c
+++ app_broker/copro_task.c
@@ -14,9 +14,13 @@
 #include <uapi/tfm_ioctl_api.h>
 #include <errno.h>
 
+#include "copro_task.h"
+
 #define MSEC_PER_SEC		1000L
 
 #define COPRO_TIMEOUT_MS	1 * MSEC_PER_SEC
+
+#define COPRO_ID_A35		0
 
 __WEAK const char *cpu_status_str[] = {
 	"offline",
@@ -27,7 +31,7 @@ __WEAK const char *cpu_status_str[] = {
 	"unknow",
 };
 
-static int32_t copro_wait_status(struct cpu_info_res *cpu_info,
+static int32_t copro_wait_status(uint32_t cpu_id, struct cpu_info_res *cpu_info,
 				 uint32_t timeout_ms, int32_t status)
 {
 	uint32_t tk_timeout, tk_start, tk_curr;
@@ -38,7 +42,7 @@ static int32_t copro_wait_status(struct cpu_info_res *cpu_info,
 	tk_start = osKernelGetTickCount();
 
 	do {
-		err = tfm_platform_cpu_info(0, cpu_info);
+		err = tfm_platform_cpu_info(cpu_id, cpu_info);
 		if (err != TFM_PLATFORM_ERR_SUCCESS) {
 			cpu_info->status = CPU_LAST;
 			break;
@@ -54,57 +58,80 @@ static int32_t copro_wait_status(struct cpu_info_res *cpu_info,
 	return (cpu_info->status == status) ? 0 : -ETIMEDOUT;
 }
 
-void copro_start_task(void *argument)
+typedef enum tfm_platform_err_t (*cpu_cmd_fn_t)(uint32_t, int32_t *);
+
+static void _copro_cmd_wait(cpu_cmd_fn_t cpu_fn, uint32_t cpu_id, int32_t wait_status)
 {
-	struct cpu_info_res cpu_info;
+        struct cpu_info_res cpu_info;
 	int32_t status, err;
 
-	UNUSED_VARIABLE(argument);
+	if (!cpu_fn)
+		return;
 
-	err = tfm_platform_cpu_info(0, &cpu_info);
+	err = tfm_platform_cpu_info(cpu_id, &cpu_info);
 	if (err != TFM_PLATFORM_ERR_SUCCESS) {
-		LOG_MSG("[NS] [ERR] get cpu 0 info fail: %d\r\n", err);
-		goto out;
+		LOG_MSG("[NS] [COPRO] [ERR] get cpu 0 info fail: %d\r\n", err);
+		return;
 	}
 
 	if (cpu_info.status < 0 || cpu_info.status >= CPU_LAST) {
-		LOG_MSG("[NS] [ERR] cpu %s error %d\r\n",
+		LOG_MSG("[NS] [COPRO] [ERR] cpu %s error %d\r\n",
                         cpu_info.name, cpu_info.status);
-		goto out;
+		return;
 	}
 
-	if (cpu_info.status != CPU_OFFLINE) {
-		LOG_MSG("[NS] [INF] cpu %s already started\r\n", cpu_info.name);
-		LOG_MSG("[NS] [INF] cpu %s status: %s\r\n",
+	if (cpu_info.status == wait_status) {
+		LOG_MSG("[NS] [COPRO] [INF] cpu %s is already %s\r\n",
 			cpu_info.name, cpu_status_str[cpu_info.status]);
-		goto out;
+		return;
 	}
 
-	err = tfm_platform_cpu_start(0, &status);
+	err = cpu_fn(cpu_id, &status);
 	if (err != TFM_PLATFORM_ERR_SUCCESS) {
-		LOG_MSG("[NS] [ERR] cpu %s start fail err: %d\r\n", cpu_info.name, err);
-		goto out;
+		LOG_MSG("[NS] [COPRO] [ERR] cpu %s cmd fail: %d\r\n", cpu_info.name, err);
+		return;
 	}
 
-	err = copro_wait_status(&cpu_info, COPRO_TIMEOUT_MS, CPU_RUNNING);
-	if (err) {
-		LOG_MSG("[NS] [ERR] cpu %s not running status: %s\r\n",
-			cpu_info.name, cpu_status_str[status]);
-
-		err = tfm_platform_cpu_stop(0, &status);
-		if (err)
-			LOG_MSG("[NS] [ERR] cpu %s stop fail err: %d\r\n", cpu_info.name, err);
-
-		goto out;
+	err = copro_wait_status(cpu_id, &cpu_info, COPRO_TIMEOUT_MS, wait_status);
+	if (err == -ETIMEDOUT) {
+		LOG_MSG("[NS] [COPRO] [ERR] cpu %s timeout\r\n", cpu_info.name);
+		return;
 	}
 
-	LOG_MSG("[NS] [INF] cpu %s started\r\n", cpu_info.name);
-
-out:
-	osThreadExit();
+	LOG_MSG("[NS] [COPRO] [INF] cpu %s now %s\r\n",
+		cpu_info.name, cpu_status_str[cpu_info.status]);
 }
 
-static osThreadFunc_t copro_thread_func = copro_start_task;
+static void _copro_start(void)
+{
+	_copro_cmd_wait(tfm_platform_cpu_start, COPRO_ID_A35, CPU_RUNNING);
+}
+
+static void _copro_stop(void)
+{
+	_copro_cmd_wait(tfm_platform_cpu_stop, COPRO_ID_A35, CPU_OFFLINE);
+}
+
+void copro_ctrl_task(void *argument)
+{
+	uint32_t cmd;
+
+	UNUSED_VARIABLE(argument);
+
+	for (;;) {
+		cmd = osThreadFlagsWait(COPRO_START | COPRO_STOP, osFlagsWaitAny, osWaitForever);
+		if (cmd == COPRO_START)
+			_copro_start();
+		else if (cmd == COPRO_STOP)
+			_copro_stop();
+		else
+			LOG_MSG("[NS] [COPRO] [ERR] unknown cmd\r\n");
+	}
+}
+
+osThreadId_t tid_copro = NULL;
+
+static osThreadFunc_t copro_thread_func = copro_ctrl_task;
 static const osThreadAttr_t copro_thread_attr = {
     .name = "copro_thread",
     .stack_size = 1024U,
@@ -114,7 +141,9 @@ static const osThreadAttr_t copro_thread_attr = {
 
 int copro_init(void)
 {
-	osThreadNew(copro_thread_func, NULL, &copro_thread_attr);
+	tid_copro = osThreadNew(copro_thread_func, NULL, &copro_thread_attr);
+
+	osThreadFlagsSet(tid_copro, COPRO_START);
 
 	return 0;
 }
--- app_broker/copro_task.h
+++ app_broker/copro_task.h
@@ -8,6 +8,11 @@
 #ifndef __TFM_COPRO_TASK_H__
 #define __TFM_COPRO_TASK_H__
 
+#define COPRO_START	(0x1U)
+#define COPRO_STOP	(0x2U)
+
+extern osThreadId_t tid_copro;
+
 int copro_init(void);
 
 #endif /* __TFM_COPRO_TASK_H__ */
